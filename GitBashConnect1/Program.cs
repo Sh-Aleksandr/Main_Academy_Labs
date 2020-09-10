0a5052e06490340b20be16edb1e634d99b57892b
wwwblogger1 <wwwblogger1@gmail.com>
2020-09-10 19:39:56 +0200

    Добвил строку

--- GitBashConnect1/Program.cs
+++ GitBashConnect1/Program.cs
@@ -6,6 +6,7 @@ namespace GitBashConnect1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hi");
             Console.WriteLine("Hello World!");
         }
     }
