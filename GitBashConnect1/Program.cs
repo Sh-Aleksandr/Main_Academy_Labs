b31cb13cd9cfae1cff9a457ea2ba3c0eed683206
wwwblogger1 <wwwblogger1@gmail.com>
2020-10-18 18:57:32 +0300

    my first commit

--- GitBashConnect1/Program.cs
+++ GitBashConnect1/Program.cs
@@ -7,7 +7,7 @@ namespace GitBashConnect1
         static void Main(string[] args)
         {
             Console.WriteLine("Hi");
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!  ");
             Console.WriteLine("Good");
         }
     }
